5f415a6a02b7a43def6b299181d1bf31019f6c8f
Alejandra Barrientos Grisales <abarrientos@unal.edu.co>
2022-11-04 18:17:06 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -15,10 +15,40 @@ import re
 
 def ingest_data():
 
+    with open('clusters_report.txt') as report:
+        linea = report.readlines()
+ 
+    linea = linea[4:]
 
-    msje = "hola"
+    lista = [0, 0, 0, ""]
+    lista2= []
     
-    return msje
 
-print(ingest_data())
+    for elem in linea:
+        if re.match('^ +[0-9]+ +', elem):
+            num, cant, porc, *pal = elem.split()
+    
+            lista[0] = int(num)
+            lista[1] = int(cant)
+            lista[2] = float(porc.replace(",",".")) 
+
+            pal.pop(0) 
+            pal = ' '.join(pal)
+            lista[3] += pal
+
+        if re.match("^ +[a-z]", elem):
+            pal = elem.split()
+            pal = " ".join(pal)
+            lista[3] += " " + pal
+
+        if re.match("^\n", elem) or re.match("^ +$", elem):
+            lista[3] = lista[3].replace(",",".") 
+            lista2.append(lista)
+            lista = [0, 0, 0, ""]
+
+    salida = pd.DataFrame(lista2, columns = ["cluster", "cantidad_de_palabras_clave", "porcentaje_de_palabras_clave", "principales_palabras_clave"])
+    
+    return salida
+
+
     
